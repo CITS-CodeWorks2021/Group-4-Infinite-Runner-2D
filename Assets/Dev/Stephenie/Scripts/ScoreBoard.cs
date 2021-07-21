25b721621bc2492403f9b7b1538514147f0bbd16
Steph3nie <86668009+Steph3nie@users.noreply.github.com>
2021-07-21 14:04:04 -0400

    Scoreboard Script Basically Done
    Now, all that's left is linking the script to the game and adding an input field allowing the player to input their name.

--- Assets/Dev/Stephenie/Scripts/ScoreBoard.cs
+++ Assets/Dev/Stephenie/Scripts/ScoreBoard.cs
@@ -7,56 +7,40 @@ public class ScoreBoard : MonoBehaviour
 {
     private Transform entryContainer;
     private Transform entryTemplate;
-    private List<HighscoreEntry> highscoreEntryList;
     private List<Transform> highscoreEntryTransformList;
 
     private void Awake()
     {
         entryContainer = transform.Find("Score Board Entry Container");
         entryTemplate = entryContainer.Find("Score Board Entry Template");
-        Debug.Log(highscoreEntryList);
-        Debug.Log(highscoreEntryTransformList);
+
         entryTemplate.gameObject.SetActive(false);
-
-        /*highscoreEntryList = new List<HighscoreEntry>()
-         {
-             new HighscoreEntry { score = 20, name = "Mina" },
-             new HighscoreEntry { score = 14, name = "Leslie" },
-             new HighscoreEntry { score = 3, name = "Martin" },
-             new HighscoreEntry { score = 9, name = "Ronald" },
-         };*/
-
-        PlayerPrefs.HasKey("highscoreTable");
 
         string jsonString = PlayerPrefs.GetString("highscoreTable");
         Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+        
 
-        for (int i = 0; i < highscoreEntryList.Count; i++)
+        for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
         {
-            for (int j = i + 1; j < highscoreEntryList.Count; j++)
+            for (int j = i + 1; j < highscores.highscoreEntryList.Count; j++)
             {
-                if(highscoreEntryList[j].score > highscoreEntryList[i].score)
+                if(highscores.highscoreEntryList[j].score > highscores.highscoreEntryList[i].score)
                 {
-                    HighscoreEntry tmp = highscoreEntryList[i];
-                    highscoreEntryList[i] = highscoreEntryList[j];
-                    highscoreEntryList[j] = tmp;
+                    HighscoreEntry tmp = highscores.highscoreEntryList[i];
+                    highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
+                    highscores.highscoreEntryList[j] = tmp;
                 }
             }
         }
+
         highscoreEntryTransformList = new List<Transform>();
-        foreach (HighscoreEntry highscoreEntry in highscoreEntryList)
+        foreach (HighscoreEntry highscoreEntry in highscores.highscoreEntryList)
         {
             CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
         }
-
-        
-        Highscores highscores = new Highscores { highscoreEntryList = highscoreEntryList };
-        string json = JsonUtility.ToJson(highscores);
-        PlayerPrefs.SetString("highscoreTable", json);
-        PlayerPrefs.Save();
-        Debug.Log(PlayerPrefs.GetString("highscoreTable"));
-        
     }
+
+
 
     private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
     {
@@ -90,6 +74,25 @@ public class ScoreBoard : MonoBehaviour
         transformList.Add(entryTransform);
     }
 
+    private void AddHighscoreEntry(int score, string name)
+    {
+        //Create HighscoreEntry
+        HighscoreEntry highscoreEntry = new HighscoreEntry { score = score, name = name };
+
+        //Load saved Highscores
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        //Add new entry to Highscores
+        highscores.highscoreEntryList.Add(highscoreEntry);
+
+        //Save updated Highscores
+        string json = JsonUtility.ToJson(highscores);
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+    }
+
+    [System.Serializable]
     private class Highscores
     {
         public List<HighscoreEntry> highscoreEntryList;
