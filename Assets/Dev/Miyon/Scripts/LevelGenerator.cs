9841661af5a6368fc0a1e8642eef8ff1e42ff303
Miyon1 <adamsamiyon707@gmail.com>
2021-07-22 09:58:16 -0700

    Level Chunks Generation
    random

--- Assets/Dev/Miyon/Scripts/LevelGenerator.cs
+++ Assets/Dev/Miyon/Scripts/LevelGenerator.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 200f;
+    private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 10f;
 
     [SerializeField] private Transform levelPart_Start;
     [SerializeField] private List<Transform> levelPartList;
-    [SerializeField] private Player player;
+    [SerializeField] private Transform player;
 
     private Vector3 lastEndPosition;
+    public Transform EndPosition;
+
+    public List<GameObject> spawned = new List<GameObject>();
 
 
-    private void Awake()
+    private void Start()
     {
-        lastEndPosition = levelPart_Start.Find("EndPosition").position;
-
+       
         int startingSpawnLevelParts = 5;
         for (int i = 0; i < startingSpawnLevelParts; i++)
         {
@@ -26,7 +28,8 @@ public class LevelGenerator : MonoBehaviour
 
         private void Update()
         {
-            if (Vector3.Distance(character.GetPosition(), lastEndPosition) < PLAYER_DISTANCE_SPAWN_LEVEL_PART)
+        lastEndPosition = EndPosition.position;
+        if (Vector3.Distance(player.transform.position, lastEndPosition) < PLAYER_DISTANCE_SPAWN_LEVEL_PART)
             {
                 //Spawn another level part
                 SpawnLevelPart();
@@ -37,13 +40,33 @@ public class LevelGenerator : MonoBehaviour
     {
         Transform chosenLevelPart = levelPartList[Random.Range(0, levelPartList.Count)];
         Transform lastLevelPartTransform = SpawnLevelPart(chosenLevelPart, lastEndPosition);
-        lastEndPosition = lastLevelPartTransform.Find("endposition").position;
+        //lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+        EndPosition = lastLevelPartTransform.GetComponent<LevelChunk>().EndPosition;
+        lastEndPosition = lastLevelPartTransform.GetComponent<LevelChunk>().EndPosition.position;
+        spawned.Add(lastLevelPartTransform.gameObject);
+        if (spawned.Count > 5) RemoveOldest();
     }
 
     private Transform SpawnLevelPart(Transform levelPart, Vector3 spawnPosition)
     {
         Transform levelPartTransform = Instantiate(levelPart, spawnPosition, Quaternion.identity);
         return levelPartTransform;
+    }
+
+    public void Reset()
+    {
+        foreach(GameObject g in spawned)
+        {
+            Destroy(g);
+        }
+        spawned.Clear();
+    }
+
+    public void RemoveOldest()
+    {
+        Destroy(spawned[0]);
+        spawned.RemoveAt(0);
+
     }
 
 }
